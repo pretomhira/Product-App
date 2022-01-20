a8a0de0ac4037983055347d5dbe2c5f547c0714c
Pretom <pretomdas7.pd@gmail.com>
2022-01-20 17:22:49 +0600

    First commit for the product app, Initially just added appbar with title

---
+++ lib/models/product.dart
@@ -0,0 +1,12 @@
+class Product {
+  final String id;
+  final String name;
+  final int quantity;
+  final DateTime date;
+
+  Product(
+      {required this.id,
+      required this.name,
+      required this.quantity,
+      required this.date});
+}
